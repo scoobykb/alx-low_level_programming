e8c85e9ccfbd35ca326832cb19aa6b1fdf89b46a
kabiru suleiman <suleimankabiru82@gmail.com>
2023-07-13 16:49:28 -0700

    using printf func

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * using prinf function
+ * return success using int main
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
